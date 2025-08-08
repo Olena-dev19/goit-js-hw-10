be6e09982d4e2f7bfcf0a4f54e0ce9542a91055d
Olena <gerichpingvin@gmail.com>
2025-08-08 19:56:02 +0300

    add 2 task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,3 +3,52 @@ import "flatpickr/dist/flatpickr.min.css";
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+
+const formEl = document.querySelector('.form')
+const btn = document.querySelector('.btncreate');
+const inputEl = document.querySelector('[type=number]');
+const fieldset = document.querySelector('.stateradio')
+
+let userDelay = null;
+let selectedValue = null;
+
+fieldset.addEventListener('change', (e) => {
+    if (e.target.name === 'state' && e.target.checked) {
+        selectedValue = e.target.value;
+        console.log('оновлено:', selectedValue);
+    }
+});
+
+
+formEl.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    userDelay = inputEl.value;
+
+
+    makePromise(userDelay, selectedValue )
+
+    .then(value => iziToast.success({ message: (value)}))
+    .catch(error => iziToast.error({ message: (error) }))
+    
+    formEl.reset();
+})
+
+
+
+
+const makePromise = (delay, selectedValue) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (selectedValue === 'fulfilled') {
+                resolve(`✅ Fulfilled promise in ${delay}ms`)
+            } else {
+                reject(`❌ Rejected promise in ${delay}ms`)
+            }
+        }, delay)
+    })
+
+}
+
+
+
